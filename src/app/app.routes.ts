10d545cf7a092dbd5b548f8bdb82fb4d9d44a2b7
12manel123 <manelcastellvicerezuela@gmail.com>
2023-12-08 12:29:28 +0100

    Landing about and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { ErrorPageComponent } from './pages/error-page/error-page.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/landing-page/home/home.component';
+import { AboutComponent } from './pages/landing-page/about/about.component';
 
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
-    { path: 'error', component: ErrorPageComponent },
-    { path: '**', redirectTo: '/error' },
     { path: 'landing', component: LandingPageComponent },
+    { path: '', component: LandingPageComponent ,children: [
+        {path:'', component: HomeComponent},
+        {path:'about', component: AboutComponent}
+    ]},
+    { path: 'error', component: ErrorPageComponent },
+    { path: '**', redirectTo: '/error' }
+
 ];
