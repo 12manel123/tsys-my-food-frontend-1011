51527ec7ebcbfce1726daa4d5716b014c6e657c7
12manel123 <manelcastellvicerezuela@gmail.com>
2023-12-08 10:59:02 +0100

    Error page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tsys-my-food-frontend-1011';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'error', component: ErrorPageComponent },
+    { path: '**', redirectTo: '/error' },
+    { path: 'landing', component: LandingPageComponent },
+];
