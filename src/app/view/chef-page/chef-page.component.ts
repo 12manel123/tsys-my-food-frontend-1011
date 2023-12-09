c5d186b5078fa3e9a8115b2645ff4a47bb75fdc5
12manel123 <manelcastellvicerezuela@gmail.com>
2023-12-09 20:31:01 +0100

    Chef with servicce and model

--- src/app/view/chef-page/chef-page.component.ts
+++ src/app/view/chef-page/chef-page.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-interface Dish {
-  name: string;
-}
-
-interface Card {
-  id: number;
-  time: string;
-  dishes: Dish[];
-}
+import { Card } from '../../models/dish-chef';
+import { ChefService } from '../../services/chef.service';
 
 @Component({
   selector: 'app-chef-page',
@@ -23,98 +15,60 @@ export class ChefPageComponent implements OnInit{
   currentPage: number = 1;
   totalPages: number = 1;
   itemsPerPage: number = 8;
-  simulateOrders: number = 17;
 
   cards: Card[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private chefService: ChefService,private router: Router) {}
 
   ngOnInit() {
+    
     this.updateCurrentTime();
-    for (let i = 1; i <= this.simulateOrders; i++) {
-      this.cards.push({
-        id: i,
-        time: '12:30',
-        dishes: [
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' },
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' },
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' },
-          { name: 'Plato 2' },
-          { name: 'Plato 1' }
-        ]
-      });
-    }
+    this.totalPages = Math.ceil(this.chefService.getOrdersCount() / this.itemsPerPage);
+    this.loadCurrentPageOrders();
 
     setInterval(() => {
       this.updateCurrentTime();
-    }, 1000);
+    }, 1000);  }
 
-    this.totalPages = Math.ceil(this.cards.length /8);
-  }
-
-  updateCurrentTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    this.currentTime = `${hours}:${minutes}`;
-  }
-
-  previousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
+    updateCurrentTime() {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, '0');
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      this.currentTime = `${hours}:${minutes}`;
     }
-  }
-
-  nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-    }
-  }
-
-  reloadPage() {
-    console.log('Recargar página');
-  }
-
-  closePage() {
-    this.router.navigateByUrl('/');
-  }
-
-  getCurrentPageCards(): Card[] {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-  const endIndex = startIndex + this.itemsPerPage;
   
-  const currentPageCards = this.cards.slice(startIndex, endIndex);
-
-  // Añadir tarjetas vacías si la página no tiene suficientes tarjetas
-  while (currentPageCards.length < this.itemsPerPage) {
-    currentPageCards.push({
-      id: 0,
-      time: '',
-      dishes: []
-    });
-  }
-
-  return currentPageCards;
-  }
-
-  deleteOrder(orderId: number) {
-    console.log(`Eliminar orden con ID: ${orderId}`);
-    this.cards = this.cards.filter(card => card.id !== orderId);
-    this.totalPages = Math.ceil(this.cards.length / this.itemsPerPage);
-  }
+    loadCurrentPageOrders() {
+      this.chefService.getOrdersPage(this.currentPage, this.itemsPerPage)
+        .subscribe(orders => this.cards = orders);
+    }
+  
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.loadCurrentPageOrders();
+      }
+    }
+  
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+        this.loadCurrentPageOrders();
+      }
+    }
+  
+    reloadPage() {
+      console.log('Recargar página');
+      this.loadCurrentPageOrders();
+    }
+  
+    closePage() {
+      this.router.navigateByUrl('/');
+    }
+  
+    deleteOrder(orderId: number) {
+      this.chefService.deleteOrder(orderId);
+      this.loadCurrentPageOrders();
+      this.totalPages = Math.ceil(this.chefService.getOrdersCount() / this.itemsPerPage);
+    }
 
 }
