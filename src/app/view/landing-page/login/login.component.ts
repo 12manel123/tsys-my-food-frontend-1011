e3a44f3f54ce186b963cad026823b7d31ac07e3c
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-12-11 13:46:01 +0100

    added component logo

--- src/app/view/landing-page/login/login.component.ts
+++ src/app/view/landing-page/login/login.component.ts
@@ -1,15 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { LogoComponent } from '../../../shared/logo/logo.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports:  [ ReactiveFormsModule, LogoComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+
+
+  rta: any;
+  stylebtn: any;
+  registerForm: any;
+
+
 
   http = inject(HttpClient);
 
@@ -24,6 +33,14 @@ export class LoginComponent {
     };
     return this.http.post(`${this.urlProd}`, userData);
   }
+
+  add() {
+    throw new Error('Method not implemented.');
+  }
+
+  btnValited() {
+    throw new Error('Method not implemented.');
+    }
 
   ngOnInit(): void {
 
