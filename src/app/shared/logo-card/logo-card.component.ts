35aa20d1dec4789117e0fe9d209dbd3209b3b8a0
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-12-11 11:13:16 +0100

    added component logo-card

---
+++ src/app/shared/logo-card/logo-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './logo-card.component.html',
+  styleUrl: './logo-card.component.css'
+})
+export class LogoCardComponent {
+
+}
--- src/app/view/landing-page/nav/nav.component.ts
+++ src/app/view/landing-page/nav/nav.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LogoCardComponent } from '../../../shared/logo-card/logo-card.component';
+
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LogoCardComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
