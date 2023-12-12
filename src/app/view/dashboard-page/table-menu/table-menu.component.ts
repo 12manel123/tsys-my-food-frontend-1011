1b7b876678fc98c734e46fa662c8c09fb1756b25
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-12-12 16:28:18 +0100

    conflicts are resolved

--- src/app/view/dashboard-page/table-menu/table-menu.component.ts
+++ src/app/view/dashboard-page/table-menu/table-menu.component.ts
@@ -45,7 +45,7 @@ export class TableMenuComponent {
       this.dishes = dishes;
     });
   }
-  
+
   nextPage(): void {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = this.currentPage * this.itemsPerPage;
@@ -92,7 +92,7 @@ export class TableMenuComponent {
           categoryDessert: this.newMenu.categoryDessert,
           visible: true,
         };
-  
+
         this.menusService.addMenu(newMenu);
       }
       this.resetNewMenu();
@@ -104,7 +104,7 @@ export class TableMenuComponent {
       alert('Por favor, complete todos los campos del formulario.');
     }
   }
-  
+
   validateNewMenu(): boolean {
     return (
       this.newMenu.categoryAppetizer > 0 &&
@@ -137,7 +137,7 @@ export class TableMenuComponent {
   getDishesFirst(): Observable<DishAdmin[]> {
     return this.getDishesCategory("first");
   }
-  
+
   getDishesSecond(): Observable<DishAdmin[]> {
     return this.getDishesCategory("second");
   }
@@ -185,137 +185,4 @@ export class TableMenuComponent {
     console.log(dishId);
     return this.dishesService.getDishById(dishId);
   }
-<<<<<<< HEAD
-
-
-
-
-
-
-  ngOnInit() {
-    this.dishesService.getDishes().subscribe((dishes) => {
-      this.dishes = dishes;
-    });
-
-    this.menusService.getMenus().subscribe((menus) => {
-      this.menus = menus;
-    });
-    forkJoin([this.dishesService.getDishes(), this.menusService.getMenus()]).subscribe(
-      ([dishes, menus]) => {
-        this.dishes = dishes;
-        this.menus = menus;
-        this.loadDishNames();
-      }
-    );
-  }
-
-  editMenu(menu: Menu) {
-    this.editingMenu = { ...menu };
-    this.addingMenu = true;
-  }
-
-  deleteMenu(menuId: number) {
-    this.menusService.deleteMenu(menuId);
-  }
-
-  addMenu() {
-    this.addingMenu = true;
-  }
-
-
-  submitMenuForm() {
-    if (this.validateNewMenu()) {
-      if (this.editingMenu) {
-        // Editar menú existente
-        this.editingMenu.categoryAppetizer = this.newMenu.categoryAppetizer;
-        this.editingMenu.categoryFirst = this.newMenu.categoryFirst;
-        this.editingMenu.categorySecond = this.newMenu.categorySecond;
-        this.editingMenu.categoryDessert = this.newMenu.categoryDessert;
-        this.menusService.updateMenu(this.editingMenu);
-      } else {
-        // Agregar nuevo menú
-        const newMenu: Menu = {
-          id: 0,
-          categoryAppetizer: this.newMenu.categoryAppetizer,
-          categoryFirst: this.newMenu.categoryFirst,
-          categorySecond: this.newMenu.categorySecond,
-          categoryDessert: this.newMenu.categoryDessert,
-          visible: true,
-        };
-
-        this.menusService.addMenu(newMenu);
-      }
-      this.resetNewMenu();
-      this.addingMenu = false;
-      this.editingMenu = null;
-      console.log("Test");
-      alert('Menú guardado exitosamente.');
-      this.menusService.getMenus().subscribe((menus) => {
-        this.menus = menus;
-      });
-
-    } else {
-      alert('Por favor, complete todos los campos del formulario.');
-    }
-  }
-
-
-  reloadPage() {
-    location.reload();
-  }
-
-
-  private validateNewMenu(): boolean {
-    return (
-      this.newMenu.categoryAppetizer > 0 &&
-      this.newMenu.categoryFirst > 0 &&
-      this.newMenu.categorySecond > 0 &&
-      this.newMenu.categoryDessert > 0
-    );
-  }
-
-  private resetNewMenu() {
-    this.newMenu = { id: 0, categoryAppetizer: 0, categoryFirst: 0, categorySecond: 0, categoryDessert: 0, visible: true };
-  }
-
-
-  toggleVisibility(menu: Menu) {
-    menu.visible = !menu.visible;
-    this.menusService.updateMenu(menu);
-  }
-
-  getMenuItemsDetails(itemIds: number[]): DishAdmin[] {
-    return this.dishesService.getDishDetailsByIds(itemIds);
-  }
-
-  getDishesAppetizer(): Observable<DishAdmin[]> {
-    return this.getDishesCategory("appetizer");
-  }
-  getDishesFirst(): Observable<DishAdmin[]> {
-    return this.getDishesCategory("first");
-  }
-  getDishesSecond(): Observable<DishAdmin[]> {
-    return this.getDishesCategory("second");
-  }
-  getDishesDessert(): Observable<DishAdmin[]> {
-    return this.getDishesCategory("dessert");
-  }
-  getDishesCategory(categoryType: string): Observable<DishAdmin[]> {
-    return this.dishesService.getDishes().pipe(
-      map(dishes => dishes.filter(dish => dish.category === categoryType))
-    );
-  }
-
 }
-
-interface Menu {
-  id: number;
-  categoryAppetizer: number;
-  categoryFirst: number;
-  categorySecond: number;
-  categoryDessert: number;
-  visible: boolean;
-}
-=======
-}
->>>>>>> fae178774423a6e43f022873febb9d62ca1a33e7
