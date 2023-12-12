0c1dc26705ade0ec266b8ddf38c63568b538c09c
12manel123 <manelcastellvicerezuela@gmail.com>
2023-12-12 13:07:45 +0100

    Orders admin

--- src/app/models/orders-admin.ts
+++ src/app/models/orders-admin.ts
@@ -1,2 +1,10 @@
-export interface OrdersAdmin {
+import {DishAdmin} from "./dish-admin";
+import { Slot } from "./slots-admin";
+export interface Order {
+    id: number;
+    maked: boolean;
+    slot: Slot;
+    price: number;
+    datetime: Date;
+    dishes: DishAdmin[];
 }
