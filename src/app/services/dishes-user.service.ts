fa29f863e11bf2bbb56f2ba598c2898427fe6221
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-12-13 22:14:41 +0100

    Dish requests are added to the api

--- src/app/services/dishes-user.service.ts
+++ src/app/services/dishes-user.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { DishAdmin } from '../models/dish-admin';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishesUserService {
+
+  private url = environment.apiUrl + '/api/v1/dishes/visible';
+
+  private http = inject(HttpClient)
 
   private dishes: DishAdmin[] = [
     {
@@ -109,6 +115,10 @@ export class DishesUserService {
     },
   ];
   private dishesSubject = new BehaviorSubject<DishAdmin[]>(this.dishes);
+
+  getDishesFromApi() {
+    return this.http.get<any[]>(this.url);
+   }
 
   getDishes() {
     return this.dishesSubject.asObservable();
