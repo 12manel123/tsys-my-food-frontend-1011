e5d5e07ed6fac1ede2d2399244287b6a4a428a51
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-12-18 10:08:13 +0100

    profile and user history are created

---
+++ src/app/view/user-page/historial/historial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial',
+  standalone: true,
+  imports: [],
+  templateUrl: './historial.component.html',
+  styleUrl: './historial.component.css'
+})
+export class HistorialComponent {
+
+}
